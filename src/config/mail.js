705221b3b4587aaab4186fd09017bf3b17f7d0a3
Saulo Medeiros <saulo.medeiros@g4flex.com.br>
2019-07-18 23:15:57 -0300

    Creation of email funcionality

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '3195002ce06c5d',
+        pass: '6f1a305217f8e6',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
